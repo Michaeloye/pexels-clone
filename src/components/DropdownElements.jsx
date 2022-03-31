58a9267bacd7af61739ed21f60347e120f8c372f
michaeloye <michaeloye23@gmail.com>
2022-03-31 18:37:18 +0100

    Make two components return based on 'dropdown'prop

--- src/components/DropdownElements.jsx
+++ src/components/DropdownElements.jsx
@@ -1,22 +1,58 @@
 import React from "react";
 
-function DropdownElements({ elements, top, right }) {
-  return (
-    <ul
-      className={
-        "absolute block list-none m-0 py-2 mt-0 mb-0 text-[#5e5e5e] bg-white shadow w-44 right-0 top-10 " +
-        `top-[${top}] right-['${right}']`
-      }
-    >
-      {elements.map((element) => (
-        <li key={element} className="block text-base font-semibold">
-          <a className="block w-full h-full text-left py-2 px-4 cursor-pointer">
-            {element}
-          </a>
-        </li>
-      ))}
-    </ul>
-  );
+function DropdownElements({ dropdown, elements, top, right }) {
+  if (dropdown == "explore") {
+    return (
+      <ul
+        className={
+          `absolute block list-none m-0 py-2 mt-0 mb-0 text-[#5e5e5e] bg-white shadow w-44 top-8 right-0 `
+          // `top-${top} right-${right}`
+        }
+      >
+        {elements.map((element) => (
+          <li
+            key={element}
+            className="block text-base font-semibold hover:bg-gray-200 "
+          >
+            <a className="block w-full h-full text-left py-2 px-4 cursor-pointer hover:text-black">
+              {element}
+            </a>
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    return (
+      <ul
+        className={
+          `absolute block list-none m-0 py-2 mt-0 mb-0 text-[#5e5e5e] bg-white shadow w-96 top-6 ` +
+          `top-${top} right-${right}`
+        }
+      >
+        {elements.map((element) => (
+          <li
+            key={element}
+            className="block text-base font-semibold hover:bg-gray-200 "
+          >
+            {/* The flex is because of the image to change language */}
+            <a className="flex gap-1 w-full h-full text-left py-2 px-4 cursor-pointer hover:text-black">
+              {element}{" "}
+              {element == "Change Language" ? (
+                <img
+                  width="36"
+                  height="26"
+                  src="https://www.pexels.com/assets/flags/en-US-9ac49f52fbe3cc86ef500da8d7dfac4468c0e98419808425a9cdc1af7714cee1.png"
+                />
+              ) : (
+                ""
+              )}
+            </a>
+          </li>
+        ))}
+        {/* Social handles */}
+      </ul>
+    );
+  }
 }
 
 export default DropdownElements;
