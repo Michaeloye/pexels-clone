c74c90b79b39d707663cedb0d27b5669a7c1f062
michaeloye <michaeloye23@gmail.com>
2022-03-31 04:09:34 +0100

    Add Navbar.jsx

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,83 @@
+import LogoComplete from "../LogoComplete";
+import SearchBar from "../SearchBar";
+
+function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 flex items-center bg-black px-[16.1px] min-h-[66px] z-30">
+      <div className="container flex justify-around md:justify-between items-center mx-auto">
+        <LogoComplete />
+        <SearchBar />
+
+        <div className="flex">
+          <button
+            data-collapse-toggle="mobile-menu-3"
+            type="button"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="mobile-menu-3"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <svg
+              className="hidden w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div
+          className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+          id="mobile-menu-3"
+        >
+          <ul className="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0 ">
+            <li>
+              <a href="#" className="block text-white text-lg">
+                Explore
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block text-white text-lg">
+                License
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block text-white text-lg">
+                Upload
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block text-white text-lg">
+                ...
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block text-white text-lg font-medium">
+                Join
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
