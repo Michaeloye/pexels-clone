71c1f5503f75eabc683fad35ec012b1e9fa8c495
michaeloye <michaeloye23@gmail.com>
2022-03-31 18:51:47 +0100

    Add different returns based on dropdown prop

--- src/components/SmallTriangle.jsx
+++ src/components/SmallTriangle.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 
 // This represents the small triangle that comes out when a user hovers on explore on the navbar
-function SmallTriangle({ top, right }) {
+function SmallTriangle({ dropdown, top, right }) {
   // The props are used to position the component
-  return (
-    <div
-      className={
-        "absolute w-[15px] h-[15px] bg-white rounded-sm top-9 right-5 " +
-        `top-['${top}'] right-['${right}']`
-      }
-      style={{
-        content: '""',
-        WebkitTransform: "rotate(45deg)",
-        transform: "rotate(45deg)",
-        boxShadow: "-3px -3px 5px rgba(82,95,127,.04)",
-      }}
-    ></div>
-  );
+  if (dropdown == "explore") {
+    return (
+      <div
+        className={
+          "absolute w-[15px] h-[15px] bg-white rounded-sm top-8 right-5 "
+          // `top-${top} right-${right}`
+        }
+        style={{
+          content: '""',
+          WebkitTransform: "rotate(45deg)",
+          transform: "rotate(45deg)",
+          boxShadow: "-3px -3px 5px rgba(82,95,127,.04)",
+        }}
+      ></div>
+    );
+  } else {
+    return (
+      <div
+        className={
+          "absolute w-[15px] h-[15px] bg-white rounded-sm top-7 right-1 "
+          // `top-${top} right-${right}`
+        }
+        style={{
+          content: '""',
+          WebkitTransform: "rotate(45deg)",
+          transform: "rotate(45deg)",
+          boxShadow: "-3px -3px 5px rgba(82,95,127,.04)",
+        }}
+      ></div>
+    );
+  }
 }
 
 export default SmallTriangle;
