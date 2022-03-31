e8c630bb17d31c7c1711b2ea40b2ee0de7c4be2c
michaeloye <michaeloye23@gmail.com>
2022-04-01 00:03:43 +0100

    Add Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TopSection from "../components/TopSection/TopSection";
+
+function Home() {
+  return (
+    <div>
+      <TopSection />
+    </div>
+  );
+}
+
+export default Home;
