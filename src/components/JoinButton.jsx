28a566954592d568044f231c10b5498210167c77
michaeloye <michaeloye23@gmail.com>
2022-03-31 05:24:27 +0100

    Add JoinButton.jsx

---
+++ src/components/JoinButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function JoinButton() {
+  return (
+    <a
+      href="#"
+      className="block text-white text-lg font-semibold px-7 py-[6px] rounded-[4px] bg-primary-green"
+    >
+      Join
+    </a>
+  );
+}
+
+export default JoinButton;
