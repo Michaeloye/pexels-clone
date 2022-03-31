0533629a48cc2ebcc84d8d509050053aafbb5dd3
michaeloye <michaeloye23@gmail.com>
2022-04-01 00:01:40 +0100

    Add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
