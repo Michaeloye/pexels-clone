f95c2097e12c8cfbba2e98366f2e527e0f896721
michaeloye <michaeloye23@gmail.com>
2022-03-31 05:23:56 +0100

    Add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
-import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
