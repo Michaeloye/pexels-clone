13de9cc84577907d71fc735718f4867b8d7b542d
michaeloye <michaeloye23@gmail.com>
2022-04-01 21:22:09 +0100

    Add Pages; Include react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Tabs from "./components/Tabs";
+import TopSection from "./components/TopSection/TopSection";
+import Challenges from "./pages/Challenges";
+import Discover from "./pages/Discover";
 import Home from "./pages/Home";
+import Leaderboard from "./pages/Leaderboard";
+import Videos from "./pages/Videos";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <TopSection />
+        <Tabs />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/discover" element={<Discover />} />
+          <Route exact path="/videos" element={<Videos />} />
+          <Route exact path="/leaderboard" element={<Leaderboard />} />
+          <Route exact path="/challenges" element={<Challenges />} />
+        </Routes>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </BrowserRouter>
     </div>
   );
 }
