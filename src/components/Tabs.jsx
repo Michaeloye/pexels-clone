02b2c3098db9dc88de7e4b380a7d28b9b2e88425
michaeloye <michaeloye23@gmail.com>
2022-04-01 21:21:00 +0100

    Add Tabs.jsx

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function Tabs() {
+  // the useLocation is used to get the current url pathname
+  const location = useLocation().pathname;
+  return (
+    <div className="shadow block text-center overflow-x-auto whitespace-nowrap relative">
+      {/* Map throught the different tab */}
+      {["Home", "Discover", "Videos", "Leaderboard", "Challenges"].map(
+        (element) => (
+          <Link
+            key={element}
+            to={element === "Home" ? "" : `/${element.toLowerCase()}`}
+            className="relative inline-flex text-[17px] leading-[26px] font-semibold mt-0 mb-0 tracking-[0.4px] items-center p-[17.5px] cursor-pointer no-underline"
+          >
+            {element}
+            {/* home has to be done for specially because it has a route of '/' */}
+            {element.toLowerCase() === "home" && location.slice(1) === "" ? (
+              <span className="absolute h-[3px] round-tl-sm rounded-tr-sm bottom-0 left-0 right-0 bg-primary-blue"></span>
+            ) : (
+              ""
+            )}
+            {/* location contains '/' at index 0 thus slice is necessary */}
+            {element.toLowerCase() === location.slice(1) ? (
+              <span className="absolute h-[3px] round-tl-sm rounded-tr-sm bottom-0 left-0 right-0 bg-primary-blue"></span>
+            ) : (
+              ""
+            )}
+          </Link>
+        )
+      )}
+    </div>
+  );
+}
+
+export default Tabs;
