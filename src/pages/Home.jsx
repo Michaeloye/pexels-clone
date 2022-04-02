156700566348a9591b042705a69bd1ef8df03722
michaeloye <michaeloye23@gmail.com>
2022-04-02 13:30:54 +0100

    Add dropdown for 'trending'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
+import DropdownElements from "../components/DropdownElements";
+import SmallTriangle from "../components/SmallTriangle";
 
 function Home() {
   return (
@@ -19,6 +21,19 @@ function Home() {
           >
             <span className="leading-6 whitespace-nowrap">Trending</span>
             <IoMdArrowDropdown />
+            <div>
+              {/* the below div is used because of the effect of hovering even directly above the 
+                    dropdown so as to mantain the hover effect */}
+              <div className="absolute bg-blue-900 h-10 w-40 top-5 right-0"></div>
+              <SmallTriangle dropdown="explore" top={9} right={3} />
+
+              <DropdownElements
+                dropdown="trending"
+                elements={["Trending", "New"]}
+                top={9}
+                right={20}
+              />
+            </div>
           </div>
         </div>
       </div>
