efe2d62e070dbbd43c4e59fed7ddcfbabde343a0
michaeloye <michaeloye23@gmail.com>
2022-04-02 14:41:58 +0100

    Add return for trending

--- src/components/SmallTriangle.jsx
+++ src/components/SmallTriangle.jsx
@@ -3,12 +3,25 @@ import React from "react";
 // This represents the small triangle that comes out when a user hovers on explore on the navbar
 function SmallTriangle({ dropdown, top, right }) {
   // The props are used to position the component
-  if (dropdown == "explore") {
+  if (dropdown === "explore") {
     return (
       <div
         className={
           "absolute w-[15px] h-[15px] bg-white rounded-sm top-8 right-5 "
-          // `top-${top} right-${right}`
+        }
+        style={{
+          content: '""',
+          WebkitTransform: "rotate(45deg)",
+          transform: "rotate(45deg)",
+          boxShadow: "-3px -3px 5px rgba(82,95,127,.04)",
+        }}
+      ></div>
+    );
+  } else if (dropdown === "...") {
+    return (
+      <div
+        className={
+          "absolute w-[15px] h-[15px] bg-white rounded-sm top-7 right-1 "
         }
         style={{
           content: '""',
@@ -22,8 +35,7 @@ function SmallTriangle({ dropdown, top, right }) {
     return (
       <div
         className={
-          "absolute w-[15px] h-[15px] bg-white rounded-sm top-7 right-1 "
-          // `top-${top} right-${right}`
+          "absolute w-[15px] h-[15px] bg-white rounded-sm top-8 right-9 "
         }
         style={{
           content: '""',
