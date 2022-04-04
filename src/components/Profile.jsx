5ef49ae7a388f7b86edd4f2c51396ea55c87ca5b
michaeloye <michaeloye23@gmail.com>
2022-04-04 15:53:56 +0100

    Add Profile.jsx

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import avatar from "../assets/avatar.svg";
+
+function Profile({ userImageURL, userName, followers }) {
+  return (
+    <a
+      href=""
+      className="max-w-full overflow-hidden flex items-center no-underline"
+    >
+      <div className="mr-[14px] flex items-center justify-center w-10 h-10 rounded-full bg-white">
+        <img
+          src={userImageURL === "" ? avatar : userImageURL}
+          className="min-w-full h-auto rounded-full object-fill"
+        />
+      </div>
+      {/* profile text */}
+      <div className="max-w-full overflow-hidden">
+        <div className="font-semibold mt-0 mb-0 leading-[22px] whitespace-nowrap overflow-hidden text-ellipsis text-[#1a1a1a]">
+          {userName}
+        </div>
+        <div className="text-sm leading-[18px] font-light mt-0 mb-0 whitespace-nowrap overflow-hidden text-ellipsis text-[#1a1a1a]">
+          {followers} followers
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default Profile;
