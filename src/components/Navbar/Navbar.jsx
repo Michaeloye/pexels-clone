046b1fbc108b46c7900e3038c30069c2a787889e
michaeloye <michaeloye23@gmail.com>
2022-04-05 08:50:17 +0100

    Make largeScreen Nav enter Dom based on screensize

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import { FaEllipsisH } from "react-icons/fa";
 import SmallTriangle from "../SmallTriangle";
 import DropdownElements from "../DropdownElements";
 import useScroll from "../../hooks/useScroll";
+import useScreensize from "../../hooks/useScreensize";
 
 function Navbar({ handleMobileNav }) {
   const [onHoverExplore, setOnHoverExplore] = useState(false);
@@ -15,6 +16,12 @@ function Navbar({ handleMobileNav }) {
 
   // based on the scroll postion the searchBar is to be displayed on the navbar
   const [showSearchBar, setShowSearchBar] = useState(false);
+
+  // to check screensize to know when to display the desktop nav
+  // need to remove desktop nav from dom because it is affecting :last-child css property
+  // even when hidden thus it needs to be removed from the Dom entirely
+  const [largeScreen, setLargeScreen] = useState("");
+  const { width } = useScreensize();
 
   function handleExploreHover(state) {
     setOnHoverExplore(state);
@@ -32,14 +39,23 @@ function Navbar({ handleMobileNav }) {
     }
   }, [scrollYPosition]);
 
+  useEffect(() => {
+    if (width >= 1024) {
+      setLargeScreen(true);
+    } else {
+      setLargeScreen(false);
+    }
+    return () => {
+      null;
+    };
+  }, [width]);
   return (
     <nav
       className={
-        "fixed top-0 left-0 right-0 flex items-center px-[16.1px] min-h-[66px] z-50 " +
+        "main-nav-bar fixed top-0 left-0 right-0 flex items-center px-[16.1px] min-h-[66px] z-50 " +
         (showSearchBar ? "bg-primary-black" : "bg-transparent")
       }
     >
-      {/* <div className="container flex justify-around md:justify-between items-center mx-auto"> */}
       <LogoComplete />
       {/* the searchBar postion is to be maintained so as to prevent UI shift thus base
         on the show prop the style in the searchBar component will be set to display: none */}
@@ -49,7 +65,7 @@ function Navbar({ handleMobileNav }) {
         <button
           data-collapse-toggle="mobile-menu-3"
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none dark:text-gray-400"
+          className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none dark:text-gray-400"
           aria-controls="mobile-menu-3"
           aria-expanded="false"
           onClick={handleMobileNav}
@@ -80,113 +96,115 @@ function Navbar({ handleMobileNav }) {
           </svg>
         </button>
       </div>
+
       {/* Larger display */}
-      <div
-        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-        id="mobile-menu-3"
-      >
-        <ul className="flex flex-col mt-4 items-center lg:flex-row lg:gap-10 lg:mt-0 ">
-          <li>
-            {/* onMouseEnter sets the onHoverExplore state to true so as to show the dropdown
+      {largeScreen && (
+        <div
+          className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+          id="mobile-menu-3"
+        >
+          <ul className="flex flex-col mt-4 items-center lg:flex-row lg:gap-10 lg:mt-0 ">
+            <li>
+              {/* onMouseEnter sets the onHoverExplore state to true so as to show the dropdown
               onMouset */}
-            <div
-              href="#"
-              className="relative text-white"
-              onMouseOver={() => handleExploreHover(true)}
-              onMouseLeave={() => handleExploreHover(false)}
-            >
-              <a className="block text-white text-[17px] cursor-pointer hover:opacity-90">
-                Explore
-              </a>
-              {/* This is for the dropdown that show when explore is hovered */}
-              {onHoverExplore && (
-                <>
-                  {/* the below div is used because of the effect of hovering even directly above the 
+              <div
+                href="#"
+                className="relative text-white"
+                onMouseOver={() => handleExploreHover(true)}
+                onMouseLeave={() => handleExploreHover(false)}
+              >
+                <a className="block text-white text-[17px] cursor-pointer hover:opacity-90">
+                  Explore
+                </a>
+                {/* This is for the dropdown that show when explore is hovered */}
+                {onHoverExplore && (
+                  <>
+                    {/* the below div is used because of the effect of hovering even directly above the 
                     dropdown so as to mantain the hover effect */}
-                  <div className="absolute bg-transparent h-5 w-44 top-5 right-0"></div>
-                  <div>
-                    <SmallTriangle dropdown="explore" top={9} right={3} />
-                    <DropdownElements
-                      dropdown="explore"
-                      elements={[
-                        "Discover Photos",
-                        "Popular Searches",
-                        "Leaderboard",
-                        "Challenges",
-                        "Free Videos",
-                        "Pexels Blog",
-                      ]}
-                      top={9}
-                      right={0}
-                    />
-                  </div>
-                </>
-              )}
-            </div>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block text-white text-[17px] hover:opacity-90"
-            >
-              License
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block text-white text-[17px] hover:opacity-90"
-            >
-              Upload
-            </a>
-          </li>
-          <li>
-            <div
-              href="#"
-              className="relative text-white"
-              onMouseOver={() => handleDotHover(true)}
-              onMouseLeave={() => handleDotHover(false)}
-            >
+                    <div className="absolute bg-transparent h-5 w-44 top-5 right-0"></div>
+                    <div>
+                      <SmallTriangle dropdown="explore" top={9} right={3} />
+                      <DropdownElements
+                        dropdown="explore"
+                        elements={[
+                          "Discover Photos",
+                          "Popular Searches",
+                          "Leaderboard",
+                          "Challenges",
+                          "Free Videos",
+                          "Pexels Blog",
+                        ]}
+                        top={9}
+                        right={0}
+                      />
+                    </div>
+                  </>
+                )}
+              </div>
+            </li>
+            <li>
               <a
                 href="#"
                 className="block text-white text-[17px] hover:opacity-90"
               >
-                <FaEllipsisH />
+                License
               </a>
-              {onHoverDot && (
-                <>
-                  {/* the below div is used because of the effect of hovering even directly above the 
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block text-white text-[17px] hover:opacity-90"
+              >
+                Upload
+              </a>
+            </li>
+            <li>
+              <div
+                href="#"
+                className="relative text-white"
+                onMouseOver={() => handleDotHover(true)}
+                onMouseLeave={() => handleDotHover(false)}
+              >
+                <a
+                  href="#"
+                  className="block text-white text-[17px] hover:opacity-90"
+                >
+                  <FaEllipsisH />
+                </a>
+                {onHoverDot && (
+                  <>
+                    {/* the below div is used because of the effect of hovering even directly above the 
                     dropdown so as to mantain the hover effect */}
-                  <div className="absolute bg-transparent h-10 w-80 top-3 right-0"></div>
-                  <div>
-                    <SmallTriangle dropdown="..." top={0} right={3} />
-                    <DropdownElements
-                      dropdown="..."
-                      elements={[
-                        "Login",
-                        "Join",
-                        "Change Language",
-                        "Image & Video API",
-                        "App & Plugins",
-                        "FAQ",
-                        "Partnerships",
-                        "Imprint & Terms",
-                      ]}
-                      top={9}
-                      right={0}
-                    />
-                  </div>
-                </>
-              )}
-            </div>
-          </li>
-          {/* on hover of the join botton it should translate along the y-axis upward */}
-          <li className="hover:opacity-90 hover:-translate-y-[1px]">
-            <JoinButton />
-          </li>
-        </ul>
-      </div>
-      {/* </div> */}
+                    <div className="absolute bg-transparent h-10 w-80 top-3 right-0"></div>
+                    <div>
+                      <SmallTriangle dropdown="..." top={0} right={3} />
+                      <DropdownElements
+                        dropdown="..."
+                        elements={[
+                          "Login",
+                          "Join",
+                          "Change Language",
+                          "Image & Video API",
+                          "App & Plugins",
+                          "FAQ",
+                          "Partnerships",
+                          "Imprint & Terms",
+                        ]}
+                        top={9}
+                        right={0}
+                      />
+                    </div>
+                  </>
+                )}
+              </div>
+            </li>
+            {/* on hover of the join botton it should translate along the y-axis upward */}
+            <li className="hover:opacity-90 hover:-translate-y-[1px]">
+              <JoinButton />
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
