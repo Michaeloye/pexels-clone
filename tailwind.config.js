d8169a806bc08a6348c65eca7a92adfd7c7cf951
michaeloye <michaeloye23@gmail.com>
2022-04-05 02:54:54 +0100

    Add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
       colors: {
         "primary-green": "#4fa282",
         "primary-blue": "#0064f9",
+        "primary-black": "#232a34",
         "social-hover-gray": "#232a34",
       },
     },
