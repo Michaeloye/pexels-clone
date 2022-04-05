173b97ee6510bb9a32e2c6624d50679878ebd020
michaeloye <michaeloye23@gmail.com>
2022-04-05 04:53:05 +0100

    Add font weight for large screen

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -36,14 +36,14 @@ function SearchBar({ show, placeholder, size }) {
             required="required"
             type="search"
             className={
-              "block py-[8.75px] font-medium leading-[22px] my-0 flex-1 " +
+              "block py-[8.75px] font-medium lg:font-light leading-[22px] my-0 flex-1 " +
               "rounded-tl-[6px] rounded-bl-[6px] bg-transparent border-none text-[#1a1a1a] " +
               "md:py-[12.6px] md:px-[17.5px] w-0 z-10 text-[17px] rounded-[3px] align-middle "
             }
             style={{ appearance: "textfield" }}
           />
           <button className="border-none bg-none rounded-tr-[6px] rounded-br-[6px] flex items-center justify-center px-[14px] cursor-pointer z-10 text-[#5e5e5e]">
-            <GoSearch size={25} />
+            <GoSearch size={20} />
           </button>
         </div>
       </form>
