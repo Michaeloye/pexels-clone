3cc6c4b2c1f39189591789ffb85ef8103d3a7e2d
michaeloye <michaeloye23@gmail.com>
2022-04-05 04:52:46 +0100

    Adjust font size

--- src/components/LogoComplete.jsx
+++ src/components/LogoComplete.jsx
@@ -2,11 +2,11 @@ import Logo from "./Logo";
 
 function LogoComplete() {
   return (
-    <a href="/" className="flex items-center lg:gap-3">
+    <a href="/" className="flex items-center">
       <Logo />
 
       {/* if Not on medium screen size below; display text */}
-      <span className="self-center text-lg font-semibold whitespace-nowrap hidden lg:inline-block text-white hover:opacity-90">
+      <span className="self-center text-[19px] ml-[14px] font-semibold whitespace-nowrap hidden lg:inline-block text-white hover:opacity-90">
         Pexels
       </span>
     </a>
