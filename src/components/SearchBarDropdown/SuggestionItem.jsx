0358377206f3c2c71110c416dd66071bcddf548d
michaeloye <michaeloye23@gmail.com>
2022-04-07 17:59:53 +0100

    Add path to Link

--- src/components/SearchBarDropdown/SuggestionItem.jsx
+++ src/components/SearchBarDropdown/SuggestionItem.jsx
@@ -4,11 +4,14 @@ import { GoSearch } from "react-icons/go";
 
 function SuggestionItem({ title }) {
   return (
-    <Link className="bg-[#e8e8e8] inline-flex items-center whitespace-nowrap pb-[7px] pl-[19.6px] pt-[9.1px] pr-[14px] mb-[14px] rounded-[30px] no-underline">
-      <span className="text-[14px] font-bold mt-0 inline-block text-[#1a1a1a] whitespace-nowrap mr-[9.1px] mb-[3.5px]">
+    <Link
+      to="/"
+      className="bg-[#e8e8e8] inline-flex items-center whitespace-nowrap pb-[7px] pl-[19.6px] pt-[9.1px] pr-[14px] mb-[14px] rounded-[30px] no-underline mr-2"
+    >
+      <span className="text-[14px] font-bold mt-0 inline-block text-[#1a1a1a] whitespace-nowrap mr-[9.1px]">
         {title}
       </span>
-      <GoSearch size={20} />
+      <GoSearch size={15} color="gray" />
     </Link>
   );
 }
