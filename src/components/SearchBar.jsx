56408f055424c1e7f1122e91261a6358c5a113c6
michaeloye <michaeloye23@gmail.com>
2022-04-07 18:16:52 +0100

    Add onFocus attribute to input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoSearch } from "react-icons/go";
 import SearchBarDropdown from "./SearchBarDropdown/SearchBarDropdown";
 
 // This component is being used in like two places so the props are for better responsiveness
 function SearchBar({ show, placeholder, size }) {
-  // return (
-  //   <div
-  //     className={
-  //       "relative w-3/5 lg:w-1/2 lg:block " + (show ? "visible" : "invisible")
-  //     }
-  //   >
-  //     <input
-  //       type="text"
-  //       id="email-adress-icon"
-  //       className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm outline-none"
-  //       placeholder="Search..."
-  //     />
-  //   </div>
-  // );
+  const [searchOnFocus, setSearchOnFocus] = useState(false);
   return (
     // main
     <div className={"w-full max-w-[750px] " + (show ? "visible" : "invisible")}>
@@ -35,9 +22,12 @@ function SearchBar({ show, placeholder, size }) {
             className={
               "block py-[8.75px] px-[17.5px] font-medium lg:font-light leading-[22px] my-0 flex-1 " +
               "rounded-tl-[6px] rounded-bl-[6px] bg-transparent border-none text-[#1a1a1a] " +
-              "md:py-[12.6px] md:px-[17.5px] w-0 z-10 text-[17px] rounded-[3px] align-middle outline-none "
+              "md:py-[12.6px] md:px-[17.5px] w-0 z-10 text-[17px] rounded-[3px] align-middle outline-none " +
+              "focus:bg-white focus:shadow-lg"
             }
             style={{ appearance: "textfield" }}
+            onFocus={() => setSearchOnFocus(true)}
+            onBlur={() => setSearchOnFocus(false)}
           />
           <button className="border-none bg-none rounded-tr-[6px] rounded-br-[6px] flex items-center justify-center px-[14px] cursor-pointer z-10 text-[#5e5e5e]">
             <GoSearch size={20} />
@@ -45,7 +35,7 @@ function SearchBar({ show, placeholder, size }) {
         </div>
 
         {/* Searchbar dropdown */}
-        <SearchBarDropdown />
+        {searchOnFocus && <SearchBarDropdown />}
       </form>
     </div>
   );
