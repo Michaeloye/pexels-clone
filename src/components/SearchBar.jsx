f609da7c5bdd090279cb6bf44a8725d0e84d1281
michaeloye <michaeloye23@gmail.com>
2022-04-07 19:17:44 +0100

    Handle input onFocus

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,13 +5,31 @@ import SearchBarDropdown from "./SearchBarDropdown/SearchBarDropdown";
 // This component is being used in like two places so the props are for better responsiveness
 function SearchBar({ show, placeholder, size }) {
   const [searchOnFocus, setSearchOnFocus] = useState(false);
+
+  function handleBlur(e) {
+    // if the blur was because of outside focus
+    // currentTarget is the parent element, relatedTarget is the clicked element
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setSearchOnFocus(false);
+    }
+  }
   return (
     // main
     <div className={"w-full max-w-[750px] " + (show ? "visible" : "invisible")}>
       {/* search bar */}
-      <form className="relative z-10 ">
+      <form
+        className="relative z-10 "
+        onFocus={() => setSearchOnFocus(true)}
+        // onClick={() => setSearchOnFocus(true)}
+        onBlur={(e) => handleBlur(e)}
+      >
         {/* search bar container */}
-        <div className="flex bg-white bg-opacity-95 border-none rounded-[6px] z-10">
+        <div
+          className={
+            "flex bg-white bg-opacity-95 border-none rounded-[6px] z-10 " +
+            (searchOnFocus ? "shadow-lg bg-opacity-100" : "")
+          }
+        >
           <input
             autoCapitalize="none"
             autoComplete="off"
@@ -23,14 +41,17 @@ function SearchBar({ show, placeholder, size }) {
               "block py-[8.75px] px-[17.5px] font-medium lg:font-light leading-[22px] my-0 flex-1 " +
               "rounded-tl-[6px] rounded-bl-[6px] bg-transparent border-none text-[#1a1a1a] " +
               "md:py-[12.6px] md:px-[17.5px] w-0 z-10 text-[17px] rounded-[3px] align-middle outline-none " +
-              "focus:bg-white focus:shadow-lg"
+              "focus:bg-white "
             }
             style={{ appearance: "textfield" }}
-            onFocus={() => setSearchOnFocus(true)}
-            onBlur={() => setSearchOnFocus(false)}
           />
-          <button className="border-none bg-none rounded-tr-[6px] rounded-br-[6px] flex items-center justify-center px-[14px] cursor-pointer z-10 text-[#5e5e5e]">
-            <GoSearch size={20} />
+          <button
+            className={
+              "border-none bg-none rounded-tr-[6px] rounded-br-[6px] flex items-center " +
+              "justify-center px-[14px] cursor-pointer z-10 "
+            }
+          >
+            <GoSearch size={20} color={"#5e5e5e"} />
           </button>
         </div>
 
