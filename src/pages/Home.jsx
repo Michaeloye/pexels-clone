376859b68625308d908bf79b8e78e642907defc9
michaeloye <michaeloye23@gmail.com>
2022-04-07 15:39:09 +0100

    Refactor data state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,15 +15,7 @@ function Home() {
   const [pageListNum, setPageListNum] = useState([]);
   const [data, setData] = useState({
     data1: [],
-    data2: [],
-    data3: [],
-    data4: [],
-    data5: [],
   });
-  // const [data2, setData2] = useState([]);
-  // const [data3, setData3] = useState([]);
-  // const [data4, setData4] = useState([]);
-  // const [data5, setData5] = useState([]);
   const [mediumScreen, setMediumScreen] = useState("");
   // for the purpose of responsiveness the width of the page has to be accessed so as determine the...
   // number of photo columns to display for <=1024 2 photo columns are to be displayed else 3 photo columns
@@ -31,19 +23,19 @@ function Home() {
   // set page counter that adds one when user gets to bottom so map through pages
   const { scrollYPosition } = useScroll();
 
-  // useEffect(() => {
-  //   // to determine how offscreen the card is
-  //   let elem = mainRef.current;
-  //   let rect = elem.getBoundingClientRect();
-  //   // console.log(scrollY, rect.y, rect.bottom - 150, height);
-  //   // because measurements are taken from top negating the values is necessary for proper result
-  //   if (-rect.bottom >= -height - 150 && -rect.bottom <= -height) {
-  //     setPage((prevState) => prevState + 1);
-  //     // console.log("hello");
-  //   } else {
-  //     // console.log("hnla");
-  //   }
-  // }, []);
+  useEffect(() => {
+    // to determine how offscreen the card is
+    let elem = mainRef.current;
+    let rect = elem.getBoundingClientRect();
+    // console.log(scrollY, rect.y, rect.bottom - 150, height);
+    // because measurements are taken from top negating the values is necessary for proper result
+    if (-rect.bottom >= -height - 150 && -rect.bottom <= -height) {
+      setPage((prevState) => prevState + 1);
+      // console.log("hello");
+    } else {
+      // console.log("hnla");
+    }
+  }, [scrollYPosition]);
 
   // useEffect(() => {
   //   // once scroll postion gets to this point the showSearchBar is set to true
@@ -55,20 +47,29 @@ function Home() {
   //   }
   // }, [scrollYPosition]);
 
-  // useEffect(() => {
-  //   setPageListNum(range(1, page));
-  //   axios
-  //     .get(
-  //       `https://pixabay.com/api/?key=${
-  //         import.meta.env.VITE_APIKEY
-  //       }&image_type=photo&safesearch=true&page=${page}`
-  //     )
-  //     .then((res) => {
-  //       setData({ ...data, [`data${page}`]: res.data["hits"] });
-  //       console.log(data);
-  //     })
-  //     .catch((err) => console.log(err.message));
-  // }, [page]);
+  useEffect(() => {
+    setPageListNum(range(1, page));
+    console.log(page);
+    axios
+      .get(
+        `https://pixabay.com/api/?key=${
+          import.meta.env.VITE_APIKEY
+        }&image_type=photo&safesearch=true&page=${page}`
+      )
+      .then((res) => {
+        // the object in the setData represents this:
+        // if 5 data list is hard coded in data state once is page gets to add 6 it throws an
+        // error regarding assigning to null but with the below code
+        // it destructures original data assigns current page data adds data ahead as empty object
+        // e.g {...data, data1=[a,b,c]; data2=[]}
+        setData({
+          ...data,
+          [`data${page}`]: res.data["hits"],
+          [`data${page + 1}`]: [],
+        });
+      })
+      .catch((err) => console.log(err.message));
+  }, [page]);
 
   useEffect(() => {
     if (width <= 1024) {
@@ -80,21 +81,6 @@ function Home() {
       null;
     };
   }, [width]);
-  function asdf() {
-    setPage((prevState) => prevState + 1);
-    setPageListNum(range(1, page));
-    axios
-      .get(
-        `https://pixabay.com/api/?key=${
-          import.meta.env.VITE_APIKEY
-        }&image_type=photo&safesearch=true&page=${page}`
-      )
-      .then((res) => {
-        setData({ ...data, [`data${page}`]: res.data["hits"] });
-        console.log(data);
-      })
-      .catch((err) => console.log(err.message));
-  }
 
   return (
     <main
@@ -103,7 +89,6 @@ function Home() {
     >
       {/* title tabs */}
       <TitleTabs />
-      <button onClick={() => asdf()}>click</button>
       {/* images */}
       <div className="relative flex w-full">
         {/* 2 photo columns for medium screen 3 photo columns for large screen */}
@@ -113,7 +98,12 @@ function Home() {
               {pageListNum.map((datum) => (
                 <PhotoColumn
                   key={nanoid()}
-                  images={slice(data[`data${datum}`], 0, 20, 2)}
+                  images={
+                    data[`data${datum}`] === "undefinded" ||
+                    data[`data${datum}`] === "null"
+                      ? ""
+                      : slice(data[`data${datum}`], 0, 20, 2)
+                  }
                 />
               ))}
             </div>
@@ -121,18 +111,29 @@ function Home() {
               {pageListNum.map((datum) => (
                 <PhotoColumn
                   key={nanoid()}
-                  images={slice(data[`data${datum}`], 1, 20, 2)}
+                  images={
+                    data[`data${datum}`] === "undefinded" ||
+                    data[`data${datum}`] === "null"
+                      ? ""
+                      : slice(data[`data${datum}`], 1, 20, 2)
+                  }
                 />
               ))}
             </div>
           </>
         ) : (
           <>
+            {/* each time a new page is added the new photoColumn is appended */}
             <div className="flex flex-col">
               {pageListNum.map((datum) => (
                 <PhotoColumn
                   key={nanoid()}
-                  images={slice(data[`data${datum}`], 0, 20, 3)}
+                  images={
+                    data[`data${datum}`] === "undefinded" ||
+                    data[`data${datum}`] === "null"
+                      ? ""
+                      : slice(data[`data${datum}`], 0, 20, 2)
+                  }
                 />
               ))}
             </div>
@@ -140,7 +141,12 @@ function Home() {
               {pageListNum.map((datum) => (
                 <PhotoColumn
                   key={nanoid()}
-                  images={slice(data[`data${datum}`], 1, 20, 3)}
+                  images={
+                    data[`data${datum}`] === "undefinded" ||
+                    data[`data${datum}`] === "null"
+                      ? ""
+                      : slice(data[`data${datum}`], 1, 20, 2)
+                  }
                 />
               ))}
             </div>
@@ -148,12 +154,15 @@ function Home() {
               {pageListNum.map((datum) => (
                 <PhotoColumn
                   key={nanoid()}
-                  images={slice(data[`data${datum}`], 2, 20, 3)}
+                  images={
+                    data[`data${datum}`] === "undefinded" ||
+                    data[`data${datum}`] === "null"
+                      ? ""
+                      : slice(data[`data${datum}`], 2, 20, 2)
+                  }
                 />
               ))}
             </div>
-            {/* <PhotoColumn images={slice(data1, 1, 20, 3)} />
-            <PhotoColumn images={slice(data1, 2, 20, 3)} last /> */}
           </>
         )}
       </div>
