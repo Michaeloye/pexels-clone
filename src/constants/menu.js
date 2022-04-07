09034c3acf80f5b0172edd7811d0e049751a5c60
michaeloye <michaeloye23@gmail.com>
2022-04-07 22:48:42 +0100

    Add menu.js

---
+++ src/constants/menu.js
@@ -0,0 +1,22 @@
+export const Menu = [
+  {
+    label: "Home",
+    to: "/",
+  },
+  {
+    label: "discover",
+    to: "/discover",
+  },
+  {
+    label: "videos",
+    to: "/videos",
+  },
+  {
+    label: "leaderboard",
+    to: "/leaderboard",
+  },
+  {
+    label: "challenges",
+    to: "/challenges",
+  },
+];
