32d172b010b1a39d57f6f6a0ef7f313c02e55327
michaeloye <michaeloye23@gmail.com>
2022-04-08 02:21:38 +0100

    Add Image modal

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,32 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { HiDownload } from "react-icons/hi";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import { FiHeart } from "react-icons/fi";
 import UserDetails from "./UserDetails";
 import { Img } from "react-image";
+import ImageModal from "../components/Modal/index";
+import MobileModal from "./Modal/MobileModal";
+import { UserContext } from "./PhotoColumn/PhotoColumn";
+import useScreensize from "../hooks/useScreensize";
 
 function Photo({ pageURL, imageURL, userImageURL, userName }) {
+  const user = useContext(UserContext);
   const [onImageHover, setOnImageHover] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [mediumScreen, setMediumScreen] = useState("");
+  const { width } = useScreensize();
+
+  useEffect(() => {
+    if (width < 1023) {
+      setMediumScreen(true);
+    } else {
+      setMediumScreen(false);
+    }
+    return () => {
+      null;
+    };
+  }, [width]);
+  const handleModal = (state) => {
+    setModalOpen(state);
+  };
 
   function handleImageHover(state) {
     setOnImageHover(state);
   }
   return (
     <div className="mb-[14px] md:mb-[20.3px] cursor-pointer">
+      {mediumScreen
+        ? modalOpen && (
+            <MobileModal
+              handleClose={handleModal}
+              image={imageURL}
+              pageURL={pageURL}
+              likes={user.likes}
+              userImageURL={userImageURL}
+              userName={userName}
+            />
+          )
+        : modalOpen && (
+            <ImageModal handleClose={handleModal} image={imageURL} />
+          )}
       <article
         className="photo-item-overlay relative block z-0 before:opacity-30 md:before:opacity-0 md:before:hover:opacity-100 "
         onMouseOver={() => handleImageHover(true)}
         onMouseLeave={() => handleImageHover(false)}
       >
-        <a href={pageURL} className="bg-[rgb(198,198,198)] h-12 max-h-fit">
-          {/* Img from react-image is an optimized image tag that helps in rendering images
-           */}
-          <Img
-            src={imageURL}
-            loader={<div className="h-40 md:h-64 w-full bg-gray-900"></div>}
-            className="block w-full max-w-full h-auto border-0"
-          />
-        </a>
+        {/* <a href={pageURL} className="bg-[rgb(198,198,198)] h-12 max-h-fit"> */}
+        {/* Img from react-image is an optimized image tag that helps in rendering images
+         */}
+        <Img
+          src={imageURL}
+          loader={<div className="h-40 md:h-64 w-full bg-gray-900"></div>}
+          className="block w-full max-w-full h-auto border-0"
+          onClick={() => handleModal(true)}
+        />
+        {/* </a> */}
         {/* poster details */}
         {onImageHover && (
           <>
